a1799fce8545cdbbeb108576df695fa29a883174
realchamanown <142647101+Taeyaefae4567@users.noreply.github.com>
2025-06-14 19:10:08 +0700

    script.js

--- script.js
+++ script.js
@@ -1,13 +1,14 @@
-const secretText = document.getElementById("secretText");
-const toggleBtn = document.getElementById("toggleBtn");
+document.addEventListener("DOMContentLoaded", function () {
+    const secretText = document.getElementById("secretText");
+    const toggleBtn = document.getElementById("toggleBtn");
 
-toggleBtn.addEventListener("click", () => {
-    secretText.classList.toggle("hidden");
+    toggleBtn.addEventListener("click", () => {
+        secretText.classList.toggle("hidden");
 
-    if (secretText.classList.contains("hidden")) {
-        toggleBtn.textContent = "show";
-    } else {
-        toggleBtn.textContent = "hide";
-    }
+        if (secretText.classList.contains("hidden")) {
+            toggleBtn.textContent = "show";
+        } else {
+            toggleBtn.textContent = "hide";
+        }
+    });
 });
-console.log("JS loaded!);
