b637e02445d27cf3d72e2f10b513ec994430f3a4
realchamanown <142647101+Taeyaefae4567@users.noreply.github.com>
2025-06-18 00:22:38 +0700

    Add files via upload

--- script.js
+++ script.js
@@ -1,14 +1,25 @@
-document.addEventListener("DOMContentLoaded", function () {
-    const secretText = document.getElementById("secretText");
-    const toggleBtn = document.getElementById("toggleBtn");
+const secretText = document.getElementById("secretText");
+const toggleBtn = document.getElementById("toggleBtn");
 
-    toggleBtn.addEventListener("click", () => {
-        secretText.classList.toggle("hidden");
+toggleBtn.addEventListener("click", () => {
+    secretText.classList.toggle("hidden");
 
-        if (secretText.classList.contains("hidden")) {
-            toggleBtn.textContent = "sybau";
-        } else {
-            toggleBtn.textContent = "huh?";
-        }
-    });
+    if (secretText.classList.contains("hidden")) {
+        toggleBtn.textContent = "show";
+    } else {
+        toggleBtn.textContent = "hide";
+    }
 });
+
+const themeBtn = document.getElementById("themeToggle");
+
+themeBtn.addEventListener("click", () => {
+    const isLightMode = document.body.classList.toggle("light-mode");
+
+    if (isLightMode) {
+        themeBtn.textContent = "เปลี่ยนเป็นโหมดมืด";
+    } else {
+        themeBtn.textContent = "เปลี่ยนเป็นโหมดสว่าง";
+    }
+});
+
